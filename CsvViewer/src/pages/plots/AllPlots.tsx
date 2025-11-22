eab56378a1cc0c3db11ae264aa44ad69e60e380c
Da Hwa <john.213steven@gmail.com>
2025-11-22 22:12:18 +0700

    fixing routes

--- CsvViewer/src/pages/plots/AllPlots.tsx
+++ CsvViewer/src/pages/plots/AllPlots.tsx
@@ -47,7 +47,7 @@ function AllPlots() {
 
   // Open conversion page (WaitingRoom)
   const handleConvertCSV = (targetType: string) => {
-    const url = `${PageRoutes.waitingroompage.path}?id=${fileId}&filename=${fileName}&filetype=${targetType}`;
+    const url = `${import.meta.env.BASE_URL}${PageRoutes.waitingroompage.path}?id=${fileId}&filename=${fileName}&filetype=${targetType}`;
     window.open(url, "_blank");
   };
 
